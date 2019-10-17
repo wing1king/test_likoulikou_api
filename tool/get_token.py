c21c0fc69131f4bba08f453ed995787284022a14
peak.zhang@idreamsky.com <zhang4475153@126.com>
2019-10-17 11:38:27 +0800

    Initial commit

--- tool/get_token.py
+++ tool/get_token.py
@@ -1,7 +1,48 @@
 import requests
-import time
+import datetime
 import unittest
-
+import time
 
 # 测试环境
-url = "http://10.72.12.19:8989"
+# base_url = "http://119.29.1.133"
+# 开发环境
+base_url = "http://10.72.17.30"
+# 预发布环境
+# base_url: https://sandbox-riko-xms.uu.cc
+# 正式环境
+# base_url: http://api-riko-xms.uu.cc
+
+#headres
+
+
+# 获取access_token
+def get_token():
+    data = {
+            'openid': '164b4571e4542ca8f2cc19776f7fd2f211111',
+            'uid': 99999999999,	# 170****2087手机号
+            'sessionid': 'a9d384dd8ca7a22ec38505ddaf03eb6605b6a62661',
+            }
+    res = requests.post(url= base_url + '/app/user/login', json=data)
+    return "Bearer " + res.json()['data']['access_token']
+
+
+headers = {
+        "Authorization": get_token(),
+        "access_type": '2',
+        "channel": '',
+        "content-type": "text/plain",
+        "deviceid": '',
+        "from": '3',
+        "sign": "7756948fcaecc936531065b2e1167a8c",
+        "version": "1.0.0"
+}
+
+
+
+
+
+
+
+
+
+
